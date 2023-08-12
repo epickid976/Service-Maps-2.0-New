3fb452a1fb45e386c11b2d3b503aed8c4934d25b
epickid976 <joseblanco0430906@gmail.com>
2023-08-12 15:49:19 -0400

    done

---
+++ Service Maps 2.0/Views/Others/Reset/ResetPassword.swift
@@ -0,0 +1,18 @@
+//
+//  ResetPassword.swift
+//  Service Maps 2.0
+//
+//  Created by Jose Blanco on 8/12/23.
+//
+
+import SwiftUI
+
+struct ResetPassword: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ResetPassword()
+}
