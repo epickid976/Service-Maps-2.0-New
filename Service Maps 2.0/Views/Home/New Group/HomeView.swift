6d770e765948cbf2c8e7947d186152b6a03eee47
epickid976 <joseblanco0430906@gmail.com>
2024-01-02 09:57:43 -0500

    Fixed Navigation Animation Bug

---
+++ Service Maps 2.0/Views/Home/New Group/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  Service Maps 2.0
+//
+//  Created by Jose Blanco on 9/11/23.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
