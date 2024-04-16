3ff5cd3e459c302d18089916a7639d2803e0de2f
epickid976 <joseblanco0430906@gmail.com>
2024-04-16 12:04:04 -0400

    Realm done, trying to fix some state bugs and crashes

---
+++ Service Maps 2.0/RealmObjects/HouseObject.swift
@@ -0,0 +1,8 @@
+//
+//  HouseClass.swift
+//  Service Maps 2.0
+//
+//  Created by Jose Blanco on 4/15/24.
+//
+
+import Foundation
---
+++ Service Maps 2.0/RealmObjects/TokenTerritoryObject.swift
@@ -0,0 +1,8 @@
+//
+//  TokenTerritoryObject.swift
+//  Service Maps 2.0
+//
+//  Created by Jose Blanco on 4/15/24.
+//
+
+import Foundation
