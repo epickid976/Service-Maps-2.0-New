c51612387ce763919fc194afcb3833fe1fbb1a12
epickid976 <joseblanco0430906@gmail.com>
2024-04-19 14:49:28 -0400

    TerritoryView completed, working on address view

---
+++ Service Maps 2.0/Structs/AddressData.swift
@@ -0,0 +1,8 @@
+//
+//  AddressData.swift
+//  Service Maps 2.0
+//
+//  Created by Jose Blanco on 4/17/24.
+//
+
+import Foundation
---
+++ Service Maps 2.0/ViewModels/AddressViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  AddressViewModel.swift
+//  Service Maps 2.0
+//
+//  Created by Jose Blanco on 4/17/24.
+//
+
+import Foundation
