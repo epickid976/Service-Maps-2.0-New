f5b0f245b0ff1578d75c6dd8938b616e00b98f85
epickid976 <joseblanco0430906@gmail.com>
2024-04-20 18:25:54 -0400

    Territory Addresses done

---
+++ Service Maps 2.0/ViewModels/AddAddressViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  AddAddressViewModel.swift
+//  Service Maps 2.0
+//
+//  Created by Jose Blanco on 4/19/24.
+//
+
+import Foundation
---
+++ Service Maps 2.0/Views/Add_Edit/AddAddressView.swift
@@ -0,0 +1,18 @@
+//
+//  AddAddressView.swift
+//  Service Maps 2.0
+//
+//  Created by Jose Blanco on 4/19/24.
+//
+
+import SwiftUI
+
+struct AddAddressView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddAddressView()
+}
