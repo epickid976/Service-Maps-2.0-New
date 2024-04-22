245e6513c77895854fd96f7b940308605d0bcb17
epickid976 <joseblanco0430906@gmail.com>
2024-04-22 11:11:31 -0400

    Territory, Address, Houses DONE

---
+++ Service Maps 2.0/Structs/HouseData.swift
@@ -0,0 +1,8 @@
+//
+//  HouseData.swift
+//  Service Maps 2.0
+//
+//  Created by Jose Blanco on 4/21/24.
+//
+
+import Foundation
---
+++ Service Maps 2.0/Structs/VisitData.swift
@@ -0,0 +1,8 @@
+//
+//  VisitData.swift
+//  Service Maps 2.0
+//
+//  Created by Jose Blanco on 4/21/24.
+//
+
+import Foundation
