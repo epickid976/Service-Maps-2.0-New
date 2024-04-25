d67077b1076cb63a15d33ed33416ac7e6565d479
epickid976 <joseblanco0430906@gmail.com>
2024-04-25 18:13:55 -0400

    Only have Token link view and small bug left

---
+++ Service Maps 2.0/Enums/LinkScreens.swift
@@ -0,0 +1,8 @@
+//
+//  LinkScreens.swift
+//  Service Maps 2.0
+//
+//  Created by Jose Blanco on 4/25/24.
+//
+
+import Foundation
---
+++ Service Maps 2.0/Managers/UniversalLinksManager.swift
@@ -0,0 +1,8 @@
+//
+//  UniversalLinksManager.swift
+//  Service Maps 2.0
+//
+//  Created by Jose Blanco on 4/25/24.
+//
+
+import Foundation
