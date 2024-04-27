00228c3ad3e6f434f885ac2f0fc6d0a0e0864b0c
epickid976 <joseblanco0430906@gmail.com>
2024-04-26 21:10:53 -0400

    Application Done (Only translations are missing)

---
+++ Service Maps 2.0/Views/LinkViews/RegisterKeyView.swift
@@ -0,0 +1,18 @@
+//
+//  RegisterKeyView.swift
+//  Service Maps 2.0
+//
+//  Created by Jose Blanco on 4/26/24.
+//
+
+import SwiftUI
+
+struct RegisterKeyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RegisterKeyView()
+}
